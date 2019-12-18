ac5a6fe6f8a5a030c63737d8484188368d87940b
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-12-18 15:44:58 +0100

    Aplicación Web

---
+++ usuarios.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$usuarios=[
+	'Alberto',
+	'Pedro',
+	'Kakaroto',
+	'Azufre',
+
+];
+
+$a=[
+	[
+		'login'=>'Alberto',
+		'password'=>'holahola',
+	],
+	[
+		'login'=>'Pedro',
+		'password'=>'holahola',
+	],
+	[
+		'login'=>'Kakaroto',
+		'password'=>'holahola',
+	],
+	[
+		'login'=>'Azufre',
+		'password'=>'holahola',
+	],
+];
